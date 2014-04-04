238b4c8615aa69a7ccf829bc05f4838d87834ff5
Cyril Sabbagh <sabbagh.cyril@gmail.com>
2014-04-04 22:26:22 +0200

    added center class

---
+++ src/Center.java
@@ -0,0 +1,40 @@
+
+public class Center {
+	
+	int R,C;
+	int S;
+	int priority;
+	boolean flag;
+	
+	
+	public int getR() {
+		return R;
+	}
+	public void setR(int r) {
+		R = r;
+	}
+	public int getC() {
+		return C;
+	}
+	public void setC(int c) {
+		C = c;
+	}
+	public int getS() {
+		return S;
+	}
+	public void setS(int s) {
+		S = s;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	} 
+}
