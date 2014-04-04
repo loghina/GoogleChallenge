5114c74091bba810574efe02e04c2e812a76de98
Thomas Rebele <thomas.rebele@googlemail.com>
2014-04-04 22:22:34 +0200

    added output function

--- src/action/Painter.java
+++ src/action/Painter.java
@@ -3,12 +3,24 @@ package action;
 import java.util.List;
 
 public class Painter {
-
 	
 	public static void paint(List<Action> actions, boolean[][] image) {
-		
 		for(Action action : actions) {
 			action.apply(image);
 		}
+	}
+	
+	public static String getOutput(List<Action> actions) {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append(actions.size());
+		builder.append("\n");
+		
+		for(Action action : actions) {
+			builder.append(action.toString());
+			builder.append("\n");
+		}
+		
+		return builder.toString();
 	}
 }
