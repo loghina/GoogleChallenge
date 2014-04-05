d794450d9fd28f80197a99d764580a652c2aa910
loghina <alexandrei.loghin@gmail.com>
2014-04-05 11:00:20 +0200

    nothing changed

--- src/mainround/Output.java
+++ src/mainround/Output.java
@@ -5,7 +5,6 @@ public class Output {
 	// TODO: params
 	public static String getOutput(Object input) {
 		StringBuilder builder = new StringBuilder();
-		
 		// TODO: output
 		
 		return builder.toString();
