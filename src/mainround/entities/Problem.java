dbcc44297a28f729668b38b3e12b12de06319c96
Thomas Rebele <thomas.rebele@googlemail.com>
2014-04-05 12:45:36 +0200

    adding function to init List<Car> in Problem class

--- src/mainround/entities/Problem.java
+++ src/mainround/entities/Problem.java
@@ -1,13 +1,18 @@
 
 package mainround.entities;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 
 public class Problem {
 	public DefaultDirectedWeightedGraph<Intersection, Street>  graph; 
 	public HashSet<Street> visitedStreets;
 	public float timeAvailable;
+	public int numberOfCars;
+	public int startingIntersection;
 	
 	public void setVisitedStreets(int n_streets){
 		visitedStreets=new HashSet<Street>(n_streets);
@@ -15,6 +20,23 @@ public class Problem {
 	
 	public void visitStreet(Street st){
 		visitedStreets.add(st);
+	}
+	
+	/**
+	 * Inits car list with correct starting intersection
+	 * @param problem
+	 * @return
+	 */
+	public static List<Car> initCarList(Problem problem) {
+		List<Car> result = new ArrayList<Car>();
+		for(int i=0; i<problem.numberOfCars; i++) {
+			result.add(new Car(problem.getStartingIntersection()));
+		}
+		return result;
+	}
+
+	public Intersection getStartingIntersection() {
+		return Intersection.map.get(startingIntersection);
 	}
 
 }
